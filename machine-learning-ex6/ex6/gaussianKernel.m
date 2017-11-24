d3643da369e27ef7784432d9c2b758252fb4ca63
Jason <shaotao1988@gmail.com>
2017-11-23 23:02:49 -0500

    ex6
    ex6

---
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -0,0 +1,28 @@
+function sim = gaussianKernel(x1, x2, sigma)
+%RBFKERNEL returns a radial basis function kernel between x1 and x2
+%   sim = gaussianKernel(x1, x2) returns a gaussian kernel between x1 and x2
+%   and returns the value in sim
+  
+% Ensure that x1 and x2 are column vectors
+x1 = x1(:); x2 = x2(:);
+
+% You need to return the following variables correctly.
+sim = 0;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the similarity between x1
+%               and x2 computed using a Gaussian kernel with bandwidth
+%               sigma
+%machine-learning-ex6.zip
+%
+l1distance = x1-x2;
+l2distance = l1distance'*l1distance;
+sim = exp(-l2distance/(2*sigma^2));
+
+
+
+
+
+% =============================================================
+    
+end
